5e72b4ad4493eba30d1d3e20ecd604d505d75d77
Akshay Sebastian <akshaysebastian087@gmail.com>
2024-03-20 22:18:43 +0530

    client.py

--- client.py
+++ client.py
@@ -1,12 +1,16 @@
 import socketio
-import requests
+import pyreadline
+
 # Initialize SocketIO client
 sio = socketio.Client()
+name = input("Enter your name: ")
 
 # Connect to the server
 @sio.event
 def connect():
-    print("Connected to server")
+    print("Connected to chat server\n")
+    # When connected, send the user's name to the server
+    sio.emit('set_name', {'name': name})
 
 @sio.on('client_id')
 def receive_client_id(client_id):
@@ -14,7 +18,9 @@ def receive_client_id(client_id):
 
 @sio.on('names')
 def receive_names(names):
-    print(f"Connected names: {names}")
+    names_str = ' , '.join(map(str, names))
+    #print(f"IN CHAT:\033[1;36m {names}\033[0m:")
+    print(f"IN CHAT:\033[1;36m {names_str} \033[0m")
 
 @sio.on('clients')
 def receive_clients(clients):
@@ -25,23 +31,20 @@ def receive_clients(clients):
 def receive_message(data):
     sender = data['sender']
     message = data['message']
-    print(f"\033[1;32m{sender}\033[0m: {message}")
+    print(f"\033[1;32m{sender}\033[0m: {message}\n")
 
 
 # Start the client
 def start_client():
-    name = input("Enter your name: ")
-
     # Connect to the server
     sio.connect('http://localhost:5000')
-   # payload={'name': name}
-    # requests.get('http://localhost:5000/connect',params=payload)
-
 
     # Main loop to send messages
     while True:
-        message = input( "->")
-
+        message = input( "")
+        
+        print('\033[1A\033[K')  # Move cursor up and clear the line
+        print("\t\t\t\t\t\t\033[1;35m You:\033[0m", message)
         if message.lower() == 'exit':
             break
         if message.lower() == 'get_clients':
